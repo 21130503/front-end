dff363abecd658196b3933d217dda8905f2ce204
21130503 <99793142+HUUQUY1004@users.noreply.github.com>
2024-06-21 11:34:37 +0700

    blog-list

--- newspaper/src/page/home/home.jsx
+++ newspaper/src/page/home/home.jsx
@@ -5,27 +5,45 @@ import axios from "axios";
 import Story from "../../component/story/story";
 import ContentItem from "../../component/content/contentItem";
 import { images } from "../../img";
+import arrayNewsHot from "../../store/newshot";
  function Home({rss}) {
    const data = useFetch(rss)
-   console.log(data);
+   const {array} = arrayNewsHot()
+   const [blog, setBlog] = useState([])
    // const fistItem = data?.items?.shift(
    return ( 
       <div className="wrapper w-1200 mx-auto">
        <div className="hot-event w-full">
            <HotEvent className={'w-1200 mx-auto'}/>
        </div>
-           <div className="grid grid-cols-12 gap-4 h-screen mt-6">
-               
-               <div className="col-span-10">
-                   <Story defaultPage="home"/>
-    
-               </div>
-               <div className="col-span-2">
+        <div className="grid grid-cols-12 gap-4 h-max mt-6">
+            
+            <div className="col-span-10 h-max">
+                <Story defaultPage="home"/>
 
-                <img src={images.post2} alt="" />
-                <img className="mt-8" src={images.post1} alt="" />
-               </div>
-           </div>
+            </div>
+            <div className="col-span-2 h-max">
+
+            <img src={images.post2} alt="" />
+            <img className="mt-8" src={images.post1} alt="" />
+            </div>
+        </div>
+        <div className="blog-list flex mt-9 gap-4">
+        {
+            array.map((item, index)=>{
+                return <ContentItem display={index === array.length-1 ? "none": "block"} 
+                                    showTime={false} 
+                                    item={item} 
+                                    key={index}
+                                    showDescription={false}
+                                    ml_content="0"
+                                    w="270px"
+                                    h="180px"
+                                    
+                />
+            })
+        }
+        </div>
       </div>
     );
 }
