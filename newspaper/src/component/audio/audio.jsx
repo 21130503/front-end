a158d11beef686be79d56da42d99c416a95c80b6
21130503 <99793142+HUUQUY1004@users.noreply.github.com>
2024-07-06 02:18:13 +0700

    Audio hoàn chỉnh

--- newspaper/src/component/audio/audio.jsx
+++ newspaper/src/component/audio/audio.jsx
@@ -3,32 +3,55 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import './audio.scss'
 import { useEffect } from "react";
+import axios from "axios";
 function Audio({content}) {
-    const [isPlay, setIsPlay]= useState(false)
-    const [isMute, setMute]= useState(false)
-       const handleSpeak = ()=> {
-           setIsPlay(!isPlay)
-           if(!isPlay){
-            const utterance = new SpeechSynthesisUtterance(content);
-            utterance.lang = 'vi-VN'; // Ngôn ngữ, ví dụ: 'en-US' cho tiếng Anh Mỹ
-            utterance.pitch = 1; // Cao độ giọng nói (0-2)
-            utterance.rate = 1; // Tốc độ nói (0.1-10)
-            utterance.volume = 1; // Âm lượng (0-1)
-            window.speechSynthesis.speak(utterance);
-           }
-       }
+    const [tone, setTone] = useState("vi-VN-Neural2-A")
+    const [audio, setAudio] = useState(null)
+    useEffect(() => {
+        const loadVoice = async ()=>{
+            const API_KEY = "AIzaSyAp9dH95ZOph54ynFhTA7-7HuqSxy4MUAo";
+        const endpoint =
+            `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${API_KEY}`;
+        const payload = {
+            audioConfig: {
+            audioEncoding: "MP3",
+            effectsProfileId: ["small-bluetooth-speaker-class-device"],
+            pitch: 0,
+            speakingRate: 1,
+            },
+            input: {
+            text:content,
+            },
+            voice: {
+            languageCode: "vi-VN",
+            name: tone,
+            },
+        };
+        const {data} = await axios.post(endpoint,payload)
+        console.log(data);
+        const audioSrc = `data:audio/mp3;base64,${data.audioContent}`
+        setAudio(audioSrc)
+        }
+        loadVoice()
+    }, [tone]);
+    const handleToneChange = (event) => {
+        setTone(event.target.value);
+      };
+      console.log('tone: ', tone);
     return ( 
         <div className="audio bg-slate-100 px-4 py-3 flex items-center mt-4 gap-4">
-            <div className="play">
-                <button className="flex items-center justify-center btn-play p-3" onClick={()=>handleSpeak()}>
-                    <FontAwesomeIcon icon={isPlay ? faPause : faPlay}/>
-                </button>
-            </div>
-            <div className="mute">
-            <button className="flex items-center justify-center btn-mute p-3" onClick={()=>setMute(!isMute)}>
-                    <FontAwesomeIcon icon={isMute ? faVolumeOff : faVolumeHigh}/>
-                </button>
-            </div>
+           
+            <div className="flex-1">{audio && <audio controls src={audio}/>}</div>
+            <select 
+                className="w-40 capitalize py-3 px-2" 
+                name="select" 
+                id=""
+                value={tone} 
+                onChange={handleToneChange}
+            >
+                <option className="capitalize" value="vi-VN-Neural2-A">giọng nữ</option>
+                <option className="capitalize" value="vi-VN-Neural2-D">giọng nam</option>
+            </select>
         </div>
      );
 }
